fc94f2317663f373c2bfa9c76eadb627dabd4ba1
Sili-45 <gm6whmp@hhhyan.com>
2025-11-11 15:19:01 +0500

    feat: add website-image-downloader-pro-scraper/src/converters/utils.py

---
+++ website-image-downloader-pro-scraper/src/converters/utils.py
@@ -0,0 +1,6 @@
+thonimport os
+
+def create_folder_if_not_exists(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Created folder: {folder_path}")
